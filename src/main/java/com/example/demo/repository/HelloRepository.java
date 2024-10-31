e833b74ff1eeda944109008f50525412b307d6e1
Pablo Melero Alonso <p.melero.alonso@gmail.com>
2024-10-31 09:13:06 +0100

    feat: initial version of "hello-world" microservice

--- src/main/java/com/example/demo/repository/HelloRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloRepository extends JpaRepository<Greeting, Long> {
-    Greeting findById(long id);
-}
